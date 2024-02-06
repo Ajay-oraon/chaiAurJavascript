7cdfd1f76cc43d8ba1245e1abffafaa9852edca9
Ajay-oraon <52890890+Ajay-oraon@users.noreply.github.com>
2024-02-06 14:21:38 +0000

    learned basics of Datatypes

---
+++ 01Basics/02_Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+
+let score="33"
+
+console.log(typeof score);
+
+let valueNumber =Number(score);
+
+console.log(typeof valueNumber);
+
+// "33"=>33
+// "3abc"=>NaN
+//true => 1; false =>0
