d777c33c15edd2d24e7634fd95faebc700eaa4e5
Ajay-oraon <52890890+Ajay-oraon@users.noreply.github.com>
2024-02-07 11:48:52 +0000

    Datatypes summary added

---
+++ 01Basics/datatypesSummary.js
@@ -0,0 +1,16 @@
+// Primitive :
+// 7 types: string, number, Boolean,null,undefined, symbol, BigInt
+
+
+// Reference(Non primitive)
+// Array, Objects,Functions
+
+// Undefined	"undefined"
+// Null	"object" (reason)
+// Boolean	"boolean"
+// Number	"number"
+// BigInt	"bigint"
+// String	"string"
+// Symbol	"symbol"
+// Function (implements [[Call]] in ECMA-262 terms; classes are functions as well)	"function"
+// Any other object	"object"
